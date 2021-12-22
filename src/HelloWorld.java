166355dcfd6ab6005723e552db06a985bc99578b
zbyszek-bulecki <zbyszek.bulecki@gmail.com>
2021-12-22 13:44:30 +0100

    initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,21 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        int stops;
+        int fare;
+        stops = 0;
+        fare = 0;
+        stops = stops + 1;
+        fare = fare + 5;
+        stops = stops + 1;
+        fare = fare + 3;
+        stops = stops + 1;
+        fare = fare +7;
+
+        System.out.println("The bus made $"+fare+" after "+stops+" stops.");
+        // this is a one line comment
+
+        /* This is a multiple line comment
+        it just goes on and on and on and on.
+         */
+    }
+}
