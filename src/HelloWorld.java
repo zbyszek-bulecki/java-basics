bf5f968f75582974acf2ddb0dff72f27c850fff6
zbyszek-bulecki <zbyszek.bulecki@gmail.com>
2021-12-23 20:08:31 +0100

    my first class

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,26 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
 
-     int dayOfWeek = 1;
-     String schedule;
-
-     switch(dayOfWeek) {
-         case 1: schedule = "ARD";
-             break;
-         case 2: schedule = "POPMAM";
-             break;
-         case 3: schedule = "chill";
-             break;
-         case 4: schedule = "studio time";
-             break;
-         case 5: schedule = "movie night";
-             break;
-         case 6: schedule = "shopping & chores";
-             break;
-         case 7: schedule = "studio time";
-             break;
-         default: schedule = "free";
-     } System.out.println(schedule);
-
+     public double makeChange(double itemCost, double dollarsProvided) {
+         double change = dollarsProvided - itemCost;
+         return change;
+        }
     }
 }
