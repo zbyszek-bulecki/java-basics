e3e8364f757a3ec95c8e92109b9929aa93624b76
zbyszek-bulecki <zbyszek.bulecki@gmail.com>
2021-12-23 17:09:41 +0100

    shows my schedule

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,15 +1,26 @@
 public class HelloWorld {
     public static void main(String[] args) {
 
-      int dayOfTheWeek = 5;
-      boolean holiday;
+     int dayOfWeek = 1;
+     String schedule;
 
-      if (dayOfTheWeek >= 1 && dayOfTheWeek < 6) {
-          holiday = true;
-          if (!holiday) {
-              System.out.println("Sleep in!");
-          } else System.out.println("Wake up at 7:00.");
-        }
+     switch(dayOfWeek) {
+         case 1: schedule = "ARD";
+             break;
+         case 2: schedule = "POPMAM";
+             break;
+         case 3: schedule = "chill";
+             break;
+         case 4: schedule = "studio time";
+             break;
+         case 5: schedule = "movie night";
+             break;
+         case 6: schedule = "shopping & chores";
+             break;
+         case 7: schedule = "studio time";
+             break;
+         default: schedule = "free";
+     } System.out.println(schedule);
 
     }
 }
