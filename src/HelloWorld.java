871593cdaef6252b17aafe3bc335d65d5985fa1d
zbyszek-bulecki <zbyszek.bulecki@gmail.com>
2021-12-25 20:57:41 +0100

    Martingale

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,24 +1,25 @@
 public class HelloWorld {
     public static void main(String[] args) {
 
-        public int keepRolling() {
-            int dice1 = rollDice();
-            int dice2 = rollDice();
-            int dice3 = rollDice();
-            int dice4 = rollDice();
-            int dice5 = rollDice();
-            int count = 1;
-            while (!(dice1 == dice2 && dice2 == dice3 &&
-                    dice3 == dice4 && dice4 == dice5)) {
-                //we need to re-roll
-                dice1 = rollDice();
-                dice2 = rollDice();
-                dice3 = rollDice();
-                dice4 = rollDice();
-                dice5 = rollDice();
-                count = count + 1;
+        public int martingale() {
+            int money = 1000;
+            int target = 1200;
+            int bet = 10;
+            while (money > bet) {
+                boolean win = play();
+                if (win) {
+                    money += bet;
+                    bet = 10;
+                    if (money >= target) {
+                        break;
+                    }
+                } else {
+                    money -= bet;
+                    bet *= 2;
+                }
+
             }
-            return count;
+            return money;
         }
 
     }
