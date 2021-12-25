a1cce689adbdda52302e70061d0939a181ee50cc
zbyszek-bulecki <zbyszek.bulecki@gmail.com>
2021-12-25 14:15:17 +0100

    Yahtzee dice roll

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,11 +1,25 @@
 public class HelloWorld {
     public static void main(String[] args) {
 
-        public String nametagText(String name) {
-            String nameTag="Hello my name is " + name;
-            return nametagText;
+        public int keepRolling() {
+            int dice1 = rollDice();
+            int dice2 = rollDice();
+            int dice3 = rollDice();
+            int dice4 = rollDice();
+            int dice5 = rollDice();
+            int count = 1;
+            while (!(dice1 == dice2 && dice2 == dice3 &&
+                    dice3 == dice4 && dice4 == dice5)) {
+                //we need to re-roll
+                dice1 = rollDice();
+                dice2 = rollDice();
+                dice3 = rollDice();
+                dice4 = rollDice();
+                dice5 = rollDice();
+                count = count + 1;
+            }
+            return count;
         }
-
 
     }
 }
