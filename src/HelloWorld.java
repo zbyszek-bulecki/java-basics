f1f5335dc2a2879d7b9ef6d9f02731480c736bf8
zbyszek-bulecki <zbyszek.bulecki@gmail.com>
2021-12-25 11:11:28 +0100

    Fahrenheit to Celsius converter

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,11 +1,27 @@
 public class HelloWorld {
     public static void main(String[] args) {
 
-        public String nametagText(String name) {
-            String nameTag="Hello my name is " + name;
-            return nametagText;
+        /**
+         * Takes temperature in Fahrenheit and converts it to Celsius.
+         *
+         * @param fahrenheit temperature in Fahrenheit
+         * @return temperature in Celsius
+         */
+
+        public double fahrenheitToCelsius(double fahrenheit) {
+            return (fahrenheit - 32) * 5 / 9;
         }
 
+        /**
+         * Prints temperature in both Fahrenheit and Celsius degrees.
+         *
+         * @param fahrenheit Temperature in degrees Fahrenheit.
+         */
+
+        public void printTemperature(double fahrenheit) {
+            System.out.println("F: " + fahrenheit);
+            System.out.println("C: " + fahrenheitToCelsius(fahrenheit));
+        }
 
     }
 }
