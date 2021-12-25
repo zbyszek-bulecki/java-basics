5dc52a53cfb1395f792f4d2d19cdca08e0b8e1d6
zbyszek-bulecki <zbyszek.bulecki@gmail.com>
2021-12-25 14:06:19 +0100

    Yahtzee dice throw

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,20 +1,13 @@
 public class HelloWorld {
     public static void main(String[] args) {
 
-        public int rollDice() {
-            // random num between 0 and (almost) 1
-            double randomNumber=(Math.random()*6)+1;
-            return (int) randomNumber;
-        }
-        int roll1 = rollDice;
-        int roll2 = rollDice;
+        public int countBlocks(int levels){
 
-        if (roll1 != roll2) {
-            System.out.println(roll1+roll2);
-        } else {
-            int roll3 = rollDice;
-            int roll4 = rollDice;
-            System.out.println(roll1+roll2+roll3+roll4);
+            int total = 0;
+            for(int i=1; i <= levels; i++) {
+                total = total + i * i;
+            }
+            return total;
         }
 
     }
