b7be21182842488ecb509d09db812e0684ef0d05
zbyszek-bulecki <zbyszek.bulecki@gmail.com>
2021-12-27 15:38:56 +0100

    test

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,7 @@
 public class Test {
     public static void main(String[] args){
 
-        int n=5;
+        public int factorial(int n){
         int fact = 1;
         for (int i=1; i<=n; i++) {
             fact = i * fact;
